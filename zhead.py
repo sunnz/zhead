d4a7ef6401a1e3fbcc97f0b9f6a796f4b38c4076
sunnz <sunnz@users.noreply.github.com>
2014-03-16 20:04:14 -0700

    added -n argument to set linecnt.

--- zhead.py
+++ zhead.py
@@ -6,11 +6,12 @@ def main():
     linecnt = 10
 
     parser = argparse.ArgumentParser(description='Uncompress filename and print the  first 10 lines to standard output.')
+    parser.add_argument('-n', type=int, default=linecnt, metavar='COUNT', dest='linecnt')
     parser.add_argument('filename', help='filename of the compressed text file.')
     args = parser.parse_args()
 
     with gzip.open(args.filename, 'rt') as f:
-        for cnt in range(linecnt):
+        for cnt in range(args.linecnt):
             print(f.readline(), end='')
 
 if __name__ == '__main__':
